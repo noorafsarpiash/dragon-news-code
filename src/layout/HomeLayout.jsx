bfc5c9a9747abe5c819b59af3653466b00a6a64a
noorafsarpiash <noorpiash1234@gmail.com>
2025-03-31 04:31:23 +0600

    first commit

---
+++ src/layout/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const HomeLayout = () => {
+    return (
+        <>
+            <header><Header></Header></header >
+            <nav></nav>
+            <main></main>
+        </>
+    )
+}
+
+export default HomeLayout
